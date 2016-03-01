70587263119dbb79e40e28b396cbd597b5e9b720
Hamon Vitorino <hamon.vitorino@gmail.com>
2016-03-01 14:22:43 -0300

    Valor operacional adicionado ao valor decimal para ser utulizado nos calculos

--- src/price/ValorDecimal.java
+++ src/price/ValorDecimal.java
@@ -9,85 +9,93 @@ public class ValorDecimal {
 	public static final ValorDecimal CEM = new ValorDecimal(100);
 
 	private final RoundingMode ARREDONDAMENTO_PADRAO = RoundingMode.HALF_EVEN;
-	private final Integer PRECISAO_PADRAO = 10; 
+	private final Integer PRECISAO_PADRAO = 20; 
 	
-	private final Double valor;
+	private final Double valorOperacional;
 	private final Integer precisao;
 
 	public ValorDecimal(ValorDecimal decimal) {
-		this(decimal.getValor());
+		this(decimal.getValorOperacional());
 	}
 
 	public ValorDecimal(Double valor) {
 		
-		this.valor = valor; 
+		this.valorOperacional = valor; 
 		this.precisao = PRECISAO_PADRAO;
 	}
 	
 	public ValorDecimal(Integer valor) {
 		
-		this.valor = valor.doubleValue();
+		this.valorOperacional = valor.doubleValue();
 		this.precisao = 0;
 	}
 	
 	public ValorDecimal(Double valor, Integer precisao) {
 		
-		this.valor = valor; 
+		this.valorOperacional = valor; 
 		this.precisao = precisao;
 	}
 	
 	public ValorDecimal soma(ValorDecimal outroValor) {
 		
-		Double valorSoma = this.getValor() + outroValor.getValor();
+		Double valorSoma = this.getValorOperacional() + outroValor.getValorOperacional();
 	
 		return new ValorDecimal(valorSoma);
 	}
 	
 	public ValorDecimal subtrai(ValorDecimal outroValor) {
 		
-		Double valorSubtracao = this.getValor() - outroValor.getValor();
+		Double valorSubtracao = this.getValorOperacional() - outroValor.getValorOperacional();
 	
 		return new ValorDecimal(valorSubtracao);
 	}
 	
 	public ValorDecimal multiplicaPor(ValorDecimal outroValor) {
 		
-		Double valorSubtracao = this.getValor() * outroValor.getValor();
+		Double valorSubtracao = this.getValorOperacional() * outroValor.getValorOperacional();
 	
 		return new ValorDecimal(valorSubtracao);
 	}
 	
 	public ValorDecimal dividePor(ValorDecimal outroValor) {
 		
-		Double valorSubtracao = this.getValor() / outroValor.getValor();
+		Double valorSubtracao = this.getValorOperacional() / outroValor.getValorOperacional();
 	
 		return new ValorDecimal(valorSubtracao);
 	}
 	
 	public ValorDecimal potenciaDe(ValorDecimal outroValor) {
 		
-		Double valorSubtracao = Math.pow(this.getValor(), outroValor.getValor());
+		Double valorSubtracao = Math.pow(this.getValorOperacional(), outroValor.getValorOperacional());
 	
 		return new ValorDecimal(valorSubtracao);
 	}
 	
 	public Double getValor() {
 		
-		BigDecimal valorComPrecisao = new BigDecimal(this.valor.toString())
+		BigDecimal valorComPrecisao = new BigDecimal(this.valorOperacional.toString())
 				.setScale(this.getPrecisao(), ARREDONDAMENTO_PADRAO);
 		
 		return valorComPrecisao.doubleValue();
 	}
-
-	private Integer getPrecisao() {
-		return precisao;
-	}
-
+	
 	public ValorMonetario valorMonetario() {
 		return new ValorMonetario(this);
 	}
 
 	public ValorTaxa valorTaxa() {
 		return new ValorTaxa(this);
+	}
+	
+	public ValorTaxa valorInteiro() {
+		return new ValorTaxa(this);
+	}
+
+	private Double getValorOperacional() {
+		return valorOperacional;
+	}
+	
+	private Integer getPrecisao() {
+		return precisao;
 	}
 }
